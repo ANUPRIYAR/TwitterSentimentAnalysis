503df52b1bd212133f0d1c10ff5b1eafa5185923
ANUPRIYA RAMACHANDRAN <36956239+ANUPRIYAR@users.noreply.github.com>
2024-06-20 10:27:41 +0530

    Create constants.py

---
+++ fastapiproject/app/roberta_utlls/constants.py
@@ -0,0 +1,8 @@
+# CONSTANTS
+pre_trained_Robertamodel='distilroberta-base'
+batch_size=8
+Max_length= 120
+n_classes=5
+class_names = ['Extremely Negative', 'Negative', 'Neutral', 'Positive', 'Extremely Positive']
+epochs=1
+device = None
